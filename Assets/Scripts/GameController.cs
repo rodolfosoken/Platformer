13e41e44ed4be053cff139e1468936ca10e0d1d9
Rodolfo Kohei Soken <rodolfosoken@gmail.com>
2015-03-07 20:29:11 -0300

    Player Behaviour

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -34,8 +34,33 @@ new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     public Transform wall;
 
 
+    void Builtlevel()
+    {
+        //Tome os objetos Dynamicobjectscriados na cena e torne-os pais
+        GameObject dynamicParent = GameObject.Find("DynamicObjects");
+
+        //Para cada elemento neste  nivel
+        for (int yPos = 0; yPos < level.Length; yPos++)
+        {
+            for (int xPos = 0; xPos < (level[yPos]).Length; xPos++)
+            {
+                //se =1, e uma parede, se = 0, nada
+                if(level[yPos][xPos] == 1){
+                    //cria a parede
+                    Transform newObject = Instantiate(wall, new Vector3(xPos, (level.Length - yPos), 0), Quaternion.identity) as Transform;
+                    //faça com que a variavel DynamicObjects nao polua a hierarchy
+                    newObject.parent = dynamicParent.transform;
+                }
+            }
+            
+        }
+    }
+
+
 	// Use this for initialization
 	void Start () {
+
+        Builtlevel();
 	
 	}
 	
