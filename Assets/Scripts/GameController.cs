ece265501c31ef9c67f3bbcb3eb81ba6c94fc7c1
Rodolfo Kohei Soken <rodolfosoken@gmail.com>
2015-03-30 20:31:23 -0300

    Revert "Lab 2"
    This reverts commit a1f014ba32698ee942ca97b09dd43bee9051aa3b.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,40 +8,31 @@ public class GameController : MonoBehaviour {
 new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-new int[]{1, 0, 3, 3, 0, 0, 0, 0, 3, 0, 3, 3, 0, 3, 3, 0, 0, 4, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
 new int[]{1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-new int[]{1, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-new int[]{1, 0, 0, 0, 3, 3, 0, 0, 0, 0, 0, 3, 3, 3, 0, 0, 0, 0, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 new int[]{1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
 new int[]{1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1},
-new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1},
-new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1},
-new int[]{1, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 1, 1},
-new int[]{1, 0, 0, 0, 3, 3, 0, 3, 3, 0, 0, 0, 0, 0, 3, 1, 1, 1, 1},
-new int[]{1, 0, 0, 0, 3, 3, 0, 0, 0, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1},
+new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
 new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-new int[]{1, 0, 0, 0, 1, 1, 3, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
-new int[]{1, 2, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
+new int[]{1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1},
 new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 };
     public Transform wall;
-	public Transform player;
-	public Transform orb;
-	public Transform goal;
-	public ParticleSystem goalPS;
 
-	public static GameController _instance;
-	private int orbsCollected;
-	private int orbsTotal;
-
-	public GUIText scoreText;
 
     void Builtlevel()
     {
@@ -53,48 +44,13 @@ new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
         {
             for (int xPos = 0; xPos < (level[yPos]).Length; xPos++)
             {
-
-
-				Transform toCreate = null;
-				switch (level [yPos][xPos]){
-
-				case 0:
-					//nao faz nada, pois nao deve aparecer nada
-					break;
-
-				case 1:
-					toCreate = wall;
-					break;
-
-				case 2:
-					toCreate = player;
-					break;
-
-				case 3:
-					toCreate = orb;
-					break;
-
-				case 4 :
-					toCreate = goal;
-					break;
-
-				default : 
-					print("Numero Invalido: "+(level[yPos][xPos]).ToString());
-					break;
-				}
-				if(toCreate != null){
-					Transform newObject = Instantiate (toCreate, new Vector3(xPos, (level.Length - yPos), 0), Quaternion.identity) as Transform;
-
-					if(toCreate == goal){
-						goalPS = newObject.gameObject.GetComponent<ParticleSystem>();
-					}
-
-					//organiza o hierarchy
-					newObject.parent = dynamicParent.transform;
-				}
-
-
-		
+                //se =1, e uma parede, se = 0, nada
+                if(level[yPos][xPos] == 1){
+                    //cria a parede
+                    Transform newObject = Instantiate(wall, new Vector3(xPos, (level.Length - yPos), 0), Quaternion.identity) as Transform;
+                    //faça com que a variavel DynamicObjects nao polua a hierarchy
+                    newObject.parent = dynamicParent.transform;
+                }
             }
             
         }
@@ -105,31 +61,8 @@ new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	void Start () {
 
         Builtlevel();
-
-		GameObject[] orbs;
-		orbs = GameObject.FindGameObjectsWithTag("Orb");
-
-		orbsCollected = 0;
-		orbsTotal = orbs.Length;
-
-		scoreText.text = "Orbs: " + orbsCollected + "/" + orbsTotal;
 	
 	}
-
-	void Awake(){
-		_instance = this;
-		}
-
-	public void CollectedOrb(){
-		orbsCollected++;
-		scoreText.text = "Orbs: " + orbsCollected + "/" + orbsTotal;
-
-		if(orbsCollected >= orbsTotal){
-			goalPS.Play ();
-
-		}
-
-		}
 	
 	// Update is called once per frame
 	void Update () {
